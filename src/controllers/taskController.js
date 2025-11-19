08c9b237d96ff034b7a49743a25487e6a38a9511
Edward Chvainickas <chvainickas@gmail.com>
2025-11-19 07:55:58 +0000

    Implement task CRUD operations (create, read, update, delete)

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -4,7 +4,16 @@ exports.getTasks = (req, res) => {
   if (!req.session.user) {
     return res.redirect('/auth/login');
   }
-  res.render('tasks', { user: req.session.user, tasks: [] });
+
+  const query = 'SELECT * FROM tasks WHERE user_id = ? ORDER BY created_at DESC';
+
+  db.all(query, [req.session.user.id], (err, tasks) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error fetching tasks');
+    }
+    res.render('tasks', { user: req.session.user, tasks: tasks });
+  });
 };
 
 exports.searchTasks = (req, res) => {
@@ -20,18 +29,73 @@ exports.getNewTask = (req, res) => {
 };
 
 exports.postNewTask = (req, res) => {
-  // Will implement task creation
-  res.send('Create task endpoint - to be implemented');
+  if (!req.session.user) {
+    return res.redirect('/auth/login');
+  }
+
+  const { title, description } = req.body;
+  const query = 'INSERT INTO tasks (user_id, title, description) VALUES (?, ?, ?)';
+
+  db.run(query, [req.session.user.id, title, description], (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error creating task');
+    }
+    res.redirect('/tasks');
+  });
 };
 
 exports.getEditTask = (req, res) => {
-  res.send('Edit task view - to be implemented');
+  if (!req.session.user) {
+    return res.redirect('/auth/login');
+  }
+
+  const taskId = req.params.id;
+  const query = 'SELECT * FROM tasks WHERE id = ? AND user_id = ?';
+
+  db.get(query, [taskId, req.session.user.id], (err, task) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error fetching task');
+    }
+    if (!task) {
+      return res.status(404).send('Task not found');
+    }
+    res.render('edit-task', { user: req.session.user, task: task });
+  });
 };
 
 exports.postEditTask = (req, res) => {
-  res.send('Edit task endpoint - to be implemented');
+  if (!req.session.user) {
+    return res.redirect('/auth/login');
+  }
+
+  const taskId = req.params.id;
+  const { title, description } = req.body;
+  const query = 'UPDATE tasks SET title = ?, description = ?, updated_at = CURRENT_TIMESTAMP WHERE id = ? AND user_id = ?';
+
+  db.run(query, [title, description, taskId, req.session.user.id], (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error updating task');
+    }
+    res.redirect('/tasks');
+  });
 };
 
 exports.deleteTask = (req, res) => {
-  res.send('Delete task endpoint - to be implemented');
+  if (!req.session.user) {
+    return res.redirect('/auth/login');
+  }
+
+  const taskId = req.params.id;
+  const query = 'DELETE FROM tasks WHERE id = ? AND user_id = ?';
+
+  db.run(query, [taskId, req.session.user.id], (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error deleting task');
+    }
+    res.redirect('/tasks');
+  });
 };
